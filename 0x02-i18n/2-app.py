f8b980af581d908272360f1d4d61678ddcbb9df7
deantosh <deantoshdaiddoh@gmail.com>
2024-11-05 16:12:07 +0000

    task 2: solution added

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Task 0
+Setup a basic Flask app in 0-app.py:
+ - a single / route and an
+ - index.html template that simply outputs “Welcome to Holberton” as page title
+  (<title>) and "Hello world" as header (<h1>).
+
+Task 1:
+Then instantiate the Babel object in your app. Store it in a module-level
+variable named babel.
+In order to configure available languages in our app, you will create a Config
+class that has a LANGUAGES class attribute equal to ["en", "fr"].
+Use Config to set Babel’s default locale ("en") and timezone ("UTC").
+Use that class as config for your Flask app.
+
+Task 2:
+Create a get_locale function with the babel.localeselector decorator. Use
+request.accept_languages to determine the best match with our supported
+languages.
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config:
+    """APPlication configuration class."""
+    LANGUAGES = ["en", "es", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Automatically detect and return the best match with supported locales"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def index_page():
+    """Returns a welcome page"""
+    render_template(
+        'index.html', title="Welcome to Holberton", header="Hello world")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
