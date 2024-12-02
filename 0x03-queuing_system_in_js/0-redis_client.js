ccf2e819538a137552e1df36f66f4a347b328e96
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-12-02 17:56:23 +0000

    task 0: solution added

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+// Module connects to a Redis server running on localhost
+
+import { createClient } from redis;
+
+const client = createClient({
+  url: 'redis://127.0.0.1:6379'
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
