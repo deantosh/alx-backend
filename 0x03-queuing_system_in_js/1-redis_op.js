7f20de99facf2ded17f5f5995a1c6e9e5a927d2e
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-12-03 11:59:43 +0000

    task 2: solution added

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,43 @@
+// Module connects to a Redis server running on localhost
+
+import { createClient } from 'redis';
+
+const client = createClient({
+  url: 'redis://127.0.0.1:6379'
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+// function set value
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Reply: ${reply}`);
+    }
+  });
+}
+
+// Function gets value
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, value) => {
+    if (err) {
+      console.error(err);
+    }
+    else {
+      console.log(value);
+    }
+  });
+}
+
+// function calls
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
